7e06051585d8fdac915fd08ca3ac4fd964b088b2
Mohd Hussain Jafri <132386345+husasin110@users.noreply.github.com>
2026-05-06 15:53:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,67 +1,127 @@
-# Existing Resource Group
-data "azurerm_resource_group" "rg" {
-  name = "myRG-hussain"
+provider "azurerm" {
+  features {}
 }
 
-# Existing VNet
-data "azurerm_virtual_network" "vnet" {
-  name                = "myVnet"
-  resource_group_name = data.azurerm_resource_group.rg.name
+# -------------------------------
+# RESOURCE GROUP
+# -------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-demo"
+  location = "East US"
 }
 
-# Existing Subnet
-data "azurerm_subnet" "subnet" {
-  name                 = "mySubnet"
-  virtual_network_name = data.azurerm_virtual_network.vnet.name
-  resource_group_name  = data.azurerm_resource_group.rg.name
+# -------------------------------
+# VIRTUAL NETWORK
+# -------------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Existing NSG
-data "azurerm_network_security_group" "nsg" {
-  name                = "myNSG"
-  resource_group_name = data.azurerm_resource_group.rg.name
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Existing VM Public IP
-data "azurerm_public_ip" "vm_ip" {
-  name                = "vmPublicIP"
-  resource_group_name = data.azurerm_resource_group.rg.name
+# -------------------------------
+# PUBLIC IP FOR LOAD BALANCER
+# -------------------------------
+resource "azurerm_public_ip" "lb_ip" {
+  name                = "lb-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
-# NIC (new)
-resource "azurerm_network_interface" "nic" {
-  name                = "myNIC"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+# -------------------------------
+# LOAD BALANCER
+# -------------------------------
+resource "azurerm_lb" "lb" {
+  name                = "nginx-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = data.azurerm_public_ip.vm_ip.id
+  frontend_ip_configuration {
+    name                 = "frontend"
+    public_ip_address_id = azurerm_public_ip.lb_ip.id
   }
 }
 
-# Attach NSG
-resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = data.azurerm_network_security_group.nsg.id
+# -------------------------------
+# BACKEND POOL
+# -------------------------------
+resource "azurerm_lb_backend_address_pool" "bpool" {
+  name            = "backend-pool"
+  loadbalancer_id = azurerm_lb.lb.id
 }
 
-# VM (new)
+# -------------------------------
+# HEALTH PROBE (FIXED)
+# -------------------------------
+resource "azurerm_lb_probe" "probe" {
+  name                = "http-probe"
+  loadbalancer_id     = azurerm_lb.lb.id
+  protocol            = "Http"
+  port                = 80
+  request_path        = "/"
+}
+
+# -------------------------------
+# LOAD BALANCER RULE
+# -------------------------------
+resource "azurerm_lb_rule" "http_rule" {
+  name                           = "http-rule"
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "frontend"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpool.id]
+  probe_id                       = azurerm_lb_probe.probe.id
+}
+
+# -------------------------------
+# NETWORK INTERFACE
+# -------------------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+
+    # Attach to Load Balancer
+    load_balancer_backend_address_pools_ids = [
+      azurerm_lb_backend_address_pool.bpool.id
+    ]
+  }
+}
+
+# -------------------------------
+# VIRTUAL MACHINE (NGINX)
+# -------------------------------
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "nginxVM"
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  name                = "nginx-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
-
-  admin_password                  = "Password1234!"
-  disable_password_authentication = false
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
+
+  admin_password                  = "Password1234!"
+  disable_password_authentication = false
 
   os_disk {
     caching              = "ReadWrite"
@@ -75,56 +135,13 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
+  # 🔥 Install NGINX automatically
   custom_data = base64encode(<<EOF
 #!/bin/bash
-apt update
+apt update -y
 apt install -y nginx
 systemctl enable nginx
 systemctl start nginx
 EOF
   )
-}
-
-# Existing LB Public IP
-data "azurerm_public_ip" "lb_ip" {
-  name                = "lbPublicIP"
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
-# Existing Load Balancer
-data "azurerm_lb" "lb" {
-  name                = "myLB"
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
-# Existing Backend Pool
-data "azurerm_lb_backend_address_pool" "pool" {
-  name            = "backendPool"
-  loadbalancer_id = data.azurerm_lb.lb.id
-}
-
-# Attach VM to existing backend pool
-resource "azurerm_network_interface_backend_address_pool_association" "lb_assoc" {
-  network_interface_id    = azurerm_network_interface.nic.id
-  ip_configuration_name   = "internal"
-  backend_address_pool_id = data.azurerm_lb_backend_address_pool.pool.id
-}
-
-# Existing Probe
-data "azurerm_lb_probe" "probe" {
-  name            = "http-probe"
-  loadbalancer_id = data.azurerm_lb.lb.id
-}
-
-# Existing LB Rule (optional – only if NOT exists)
-# If rule already exists → COMMENT THIS BLOCK
-resource "azurerm_lb_rule" "rule" {
-  loadbalancer_id                = data.azurerm_lb.lb.id
-  name                           = "http-rule"
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  frontend_ip_configuration_name = "PublicIP"
-  backend_address_pool_ids       = [data.azurerm_lb_backend_address_pool.pool.id]
-  probe_id                       = data.azurerm_lb_probe.probe.id
 }

040eb19d32e807019017ec08a04b4ccb3edb58ed
Mohd Hussain Jafri <132386345+husasin110@users.noreply.github.com>
2026-05-06 15:01:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,175 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "myRG-hussain"
-  location = "Central India"
+provider "azurerm" {
+  features {}
+}
+
+# ✅ Use existing Resource Group
+data "azurerm_resource_group" "rg" {
+  name = "myRG-hussain"
+}
+
+# ✅ Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVnet"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# ✅ Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# ✅ Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNSG"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# ✅ Public IP for VM
+resource "azurerm_public_ip" "vm_ip" {
+  name                = "vmPublicIP"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# ✅ NIC
+resource "azurerm_network_interface" "nic" {
+  name                = "myNIC"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_ip.id
+  }
+}
+
+# ✅ Attach NSG to NIC
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# ✅ Linux VM with NGINX
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "nginxVM"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  admin_password                  = "Password1234!"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<EOF
+#!/bin/bash
+apt update
+apt install -y nginx
+systemctl enable nginx
+systemctl start nginx
+EOF
+  )
+}
+
+# ✅ Load Balancer Public IP
+resource "azurerm_public_ip" "lb_ip" {
+  name                = "lbPublicIP"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# ✅ Load Balancer
+resource "azurerm_lb" "lb" {
+  name                = "myLB"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIP"
+    public_ip_address_id = azurerm_public_ip.lb_ip.id
+  }
+}
+
+# ✅ Backend Pool
+resource "azurerm_lb_backend_address_pool" "pool" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "backendPool"
+}
+
+# ✅ Associate NIC to LB
+resource "azurerm_network_interface_backend_address_pool_association" "lb_assoc" {
+  network_interface_id    = azurerm_network_interface.nic.id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.pool.id
+}
+
+# ✅ Health Probe
+resource "azurerm_lb_probe" "probe" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "http-probe"
+  protocol        = "Tcp"
+  port            = 80
+}
+
+# ✅ LB Rule
+resource "azurerm_lb_rule" "rule" {
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "http-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIP"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.pool.id]
+  probe_id                       = azurerm_lb_probe.probe.id
 }

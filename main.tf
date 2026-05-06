ca29f57d3727d26be621048ad360d70378dde8f7
Mohd Hussain Jafri <132386345+husasin110@users.noreply.github.com>
2026-05-06 15:57:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,143 +1,66 @@
-# -------------------------------
-# RESOURCE GROUP
-# -------------------------------
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-terraform-demo"
+  name     = "example-rg"
   location = "East US"
 }
 
-# -------------------------------
-# VIRTUAL NETWORK
-# -------------------------------
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-demo"
+  name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-demo"
+  name                 = "example-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# -------------------------------
-# PUBLIC IP FOR LOAD BALANCER
-# -------------------------------
-resource "azurerm_public_ip" "lb_ip" {
-  name                = "lb-public-ip"
+resource "azurerm_public_ip" "pip" {
+  name                = "example-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-# -------------------------------
-# LOAD BALANCER
-# -------------------------------
 resource "azurerm_lb" "lb" {
-  name                = "nginx-lb"
+  name                = "example-lb"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
 
   frontend_ip_configuration {
-    name                 = "frontend"
-    public_ip_address_id = azurerm_public_ip.lb_ip.id
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.pip.id
   }
 }
 
-# -------------------------------
-# BACKEND POOL
-# -------------------------------
+# ✅ FIXED (was data → now resource)
+resource "azurerm_lb_probe" "probe" {
+  name                = "example-probe"
+  loadbalancer_id     = azurerm_lb.lb.id
+  protocol            = "Tcp"
+  port                = 80
+}
+
 resource "azurerm_lb_backend_address_pool" "bpool" {
-  name            = "backend-pool"
+  name            = "example-bpool"
   loadbalancer_id = azurerm_lb.lb.id
 }
 
-# -------------------------------
-# HEALTH PROBE (FIXED)
-# -------------------------------
-resource "azurerm_lb_probe" "probe" {
-  name                = "http-probe"
-  loadbalancer_id     = azurerm_lb.lb.id
-  protocol            = "Http"
-  port                = 80
-  request_path        = "/"
-}
-
-# -------------------------------
-# LOAD BALANCER RULE
-# -------------------------------
-resource "azurerm_lb_rule" "http_rule" {
-  name                           = "http-rule"
+resource "azurerm_lb_rule" "lbrule" {
+  name                           = "example-rule"
   loadbalancer_id                = azurerm_lb.lb.id
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
-  frontend_ip_configuration_name = "frontend"
+  frontend_ip_configuration_name = "PublicIPAddress"
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpool.id]
   probe_id                       = azurerm_lb_probe.probe.id
-}
-
-# -------------------------------
-# NETWORK INTERFACE
-# -------------------------------
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-demo"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-
-    # Attach to Load Balancer
-    load_balancer_backend_address_pools_ids = [
-      azurerm_lb_backend_address_pool.bpool.id
-    ]
-  }
-}
-
-# -------------------------------
-# VIRTUAL MACHINE (NGINX)
-# -------------------------------
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "nginx-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
-  admin_password                  = "Password1234!"
-  disable_password_authentication = false
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  # 🔥 Install NGINX automatically
-  custom_data = base64encode(<<EOF
-#!/bin/bash
-apt update -y
-apt install -y nginx
-systemctl enable nginx
-systemctl start nginx
-EOF
-  )
 }
